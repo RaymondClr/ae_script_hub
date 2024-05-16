79a441ac7769493588ae5e561f58a3c5e6ed15fe
loyoi <1019748371@qq.com>
2024-05-16 19:09:38 +0800

    在项目中显示图层源

---
+++ 项目中显示图层源/script.jsx
@@ -0,0 +1,41 @@
+function showSelectedOrActiveComp() {
+  var comp = app.project.activeItem;
+
+  if (!comp || !(comp instanceof CompItem)) {
+    return;
+  }
+
+  clearAllProjectSelections();
+
+  if (comp.selectedLayers.length === 0) {
+    comp.selected = true;
+  } else {
+    var sourceDisplayed = false;
+    for (var i = 0; i < comp.selectedLayers.length; i++) {
+      var layer = comp.selectedLayers[i];
+      if (layer.source) {
+        layer.source.selected = true;
+        sourceDisplayed = true;
+      }
+    }
+
+    if (!sourceDisplayed) {
+      comp.selected = true;
+    }
+  }
+
+  app.project.showWindow(true);
+  // 使用命令，在项目中显示图层源
+  app.executeCommand(2517);
+}
+
+/**取消项目中所有选中 */
+function clearAllProjectSelections() {
+  for (var j = 1; j <= app.project.numItems; j++) {
+    if (app.project.item(j).selected) {
+      app.project.item(j).selected = false;
+    }
+  }
+}
+
+showSelectedOrActiveComp();
